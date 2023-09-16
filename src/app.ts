22985d53fdbc4935da44a87aacbe19149902e29a
sazzad4677 <sazzad35-1898@diu.edu.bd>
2023-09-16 13:13:05 +0600

    Check linting

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express, { Application } from "express";
 
 const app: Application = express();
 
-app.use(cors()); app.use(cors()); app.use(cookieParser());
+                           app.use(cors()); app.use(cors()); app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
